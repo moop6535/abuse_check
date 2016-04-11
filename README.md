a082125ae65602ccd2fbf32f3bae8f943cfb7b26
Moop6535 <mmei@Michaels-MacBook-Pro.local>
2016-04-11 16:53:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# abuse_check
+A quick Phantom/CasperJS script I wrote to help me automate checking IP's against AbuseIPDB.com. 
+
+My first real crack at JS browser automation, so be gentle and if there is something you really don't like feel free to do with it as you wish! 
+
+##Prerequisites:
+
+[PhantomJS](http://phantomjs.org/download.html)
+
+[CasperJS](http://docs.casperjs.org/en/latest/installation.html)
+
+
+##Usage:
+```casperjs abuse_check.js [ip list file]```
+
+##Extra Argument:
+```--show=OPTION   Change verbosity. OPTIONS are 'abuse' & 'all'```
+
+##example:
+
+```casperjs abuse_check.js list_of_ip.txt --show=abuse```
+
+##NOTE
+IP file should have only 1 IP per line
+
+I would not recommend doing more than 500 IP's at a time!
---
+++ abuse_check.js
@@ -0,0 +1,137 @@
+/*
+// AbuseIpDb check
+// by Moop
+*/
+
+var casper = require('casper').create( {
+    verbose: true
+});
+
+var input = casper.cli.get(0);
+
+if(!casper.cli.has(0)) {
+    console.log("Usage: casperjs abuse_check.js [file with ip(s)]");
+    casper.exit();
+}
+
+var fs = require('fs');
+var file_h = fs.open(casper.cli.get(0), 'r');
+var lines = new Array;
+var badIp = [];
+
+var counter = 0;
+
+while(!file_h.atEnd()) {
+    lines.push(file_h.readLine()); 
+}
+
+var upTo = lines.length;
+var value = new Array;
+
+var currentIP = 0;
+var abuseCount = 0;
+
+function compareSecondColumn(a, b) {
+    if (a.abuse === b.abuse) {
+        return 0;
+    }
+    else {
+        return (a.abuse < b.abuse) ? -1 : 1;
+    }
+}
+
+
+function abuse(ip) {
+    casper.wait(75, function() {
+            var results = this.getPageContent();
+            if(results.indexOf("was not found in our database") !=-1 || results.indexOf("bingbot") !=-1) {
+                if(casper.cli.get("show") == 'all') {
+                    console.log("\n" + ip + ": ALL CLEAR!");
+                }
+            }
+            else {                
+                var number =  this.fetchText('body > div > div > div > div > p > b');
+                var whois = this.fetchText('body > div > div > div > div > div > table > tbody > tr > td');
+                if(casper.cli.get("show") == 'abuse' || casper.cli.get("show") == 'all') {
+                    console.log("\nABUSE DETECTED!!!");
+                    this.echo("IP: " + ip + "\n" + number + " \n" + whois);
+                }
+                abuseCount++;
+                if(ip.length === 10) {
+                    badIp.push({ip:ip + "      |  Abuse Reports: ", abuse: +number});
+                }
+                else if (ip.length === 11) {
+                    badIp.push({ip:ip + "     |  Abuse Reports: ", abuse: +number});
+                }
+                else if (ip.length === 12) {
+                    badIp.push({ip:ip + "    |  Abuse Reports: ", abuse: +number});
+                } 
+                else if (ip.length === 13) {
+                    badIp.push({ip:ip + "   |  Abuse Reports: ", abuse: +number});
+                } 
+                else if (ip.length === 14) {
+                    badIp.push({ip:ip + "  |  Abuse Reports: ", abuse: +number});
+                }                 
+             }
+
+             if(!casper.cli.has("show")) {
+                
+                if(lines.length > 500) {
+                    if(counter % 50 == 0) {
+                        value.push('#');
+                        console.log.apply(console, value);                         
+                    }
+                }
+                else if(lines.length > 200) {
+                    if(counter % 20 == 0) {
+                        value.push('#');
+                        console.log.apply(console, value);                         
+                    }
+                }
+                else {
+                    if(counter % 10 == 0) {
+                        value.push('#');
+                        console.log.apply(console, value);  
+                    }
+                }
+            }
+            counter++;          
+    });
+}
+
+function start(ip) {
+    this.start('https://www.abuseipdb.com', function() {
+        this.fill('form[action="/check/"]', { ip: ip }, true);
+    });
+}
+
+function check() {
+    if(currentIP < upTo) {
+        start.call(this, lines[currentIP]);
+        abuse.call(this, lines[currentIP]);
+        currentIP++;
+        this.run(check);
+    }
+    else {
+        this.echo("\nFinished!");
+        this.echo(abuseCount + " out of " + lines.length + " ip(s) have abuse reported");
+        if(badIp.length != 0) {    
+            badIp.sort(compareSecondColumn);
+            this.echo("Bad IP(s):");
+            for(i=0; i<badIp.length; i++) {
+                this.echo(badIp[i].ip + badIp[i].abuse); 
+            }
+        }
+        else {
+            this.echo("ALL IP's CLEAR");
+        }
+        file_h.close();
+        this.exit();
+    }
+}
+
+casper.start().then(function() {
+    this.echo("Checking AbuseIpDb.com");
+});
+
+casper.run(check);
